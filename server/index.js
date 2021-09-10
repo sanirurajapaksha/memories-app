33f2acc68adf8b37fa22e6a08f0b219ad602e76f
Saniru Rajapaksha <sanirurajapaksha456@gmail.com>
2021-09-10 19:20:49 +0530

    main

--- server/index.js
+++ server/index.js
@@ -1,4 +1,24 @@
-import { Express } from "express";
-import bodyParser from "body-parser";
+import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
+
+const app = express();
+
+app.use(express.json({ limit: "50mb", extended: true }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
+app.use(cors());
+
+const CONNECTION_URL =
+  "mongodb+srv://saniru_rajapaksha:WnL3LZD74wyBDACv@memories-cluster.gd90z.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 3000;
+
+const options = { useNewUrlParser: true, useUnifiedTopology: true };
+
+mongoose
+  .connect(CONNECTION_URL, options)
+  .then(() =>
+    app.listen(PORT, () => console.log("server is listening on port " + PORT))
+  )
+  .catch((error) => console.log(error.message));
+
+mongoose.set("useFindAndModify", false);
