ecb0ccd38a3ba5d3d82bebe6f39f3c4dbd3357e2
Saniru Rajapaksha <sanirurajapaksha456@gmail.com>
2021-09-13 18:29:09 +0530

    main

--- client/memories-app/components/header.js
+++ client/memories-app/components/header.js
@@ -1,13 +1,12 @@
 import React from "react";
 
-function Header(props) {
-  return (
+class Header extends React.Component {
+  return() {
     <div className="fixed flex w-full h-28 bg-indigo-500">
       <div className="flex w-full flex-row items-center justify-center">
         <span className="text-5xl font-sans font-bold">Memories App 💖</span>
       </div>
-    </div>
-  );
+    </div>;
+  }
 }
-
 export default Header;
