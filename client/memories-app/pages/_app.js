97cc52df9c94692be1e135f338ba91d84e567329
Saniru Rajapaksha <sanirurajapaksha456@gmail.com>
2021-09-13 18:19:01 +0530

    main

--- client/memories-app/pages/_app.js
+++ client/memories-app/pages/_app.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { wrapper } from "../redux/store";
 import "../styles/globals.css";
-import App from "next/app"
+import App from "next/app";
 
 const MyApp = ({ Component, pageProps }) => <Component {...pageProps} />;
 
