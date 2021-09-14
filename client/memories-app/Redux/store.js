1055c1a2d90b5a46c9884ebb940891712ffb3416
Saniru Rajapaksha <sanirurajapaksha456@gmail.com>
2021-09-15 00:30:11 +0530

    main

--- client/memories-app/Redux/store.js
+++ client/memories-app/Redux/store.js
@@ -1,10 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
 import combineReducers from "../Redux/reducers/combine";
-import postReducers from "../Redux/reducers/postReducers";
 
 const initialState = [];
 
 export const store = configureStore({
-  reducer: postReducers,
-  preloadedState: initialState,
+  reducer: combineReducers,
 });
