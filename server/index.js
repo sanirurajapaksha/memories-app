7d8f224fb242d427e047ed0ca538724ac1d0a8ab
Saniru Rajapaksha <sanirurajapaksha456@gmail.com>
2021-09-16 23:16:34 +0530

    main

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ import router from "../server/routes/Post.js";
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
 app.use("/posts", router);
