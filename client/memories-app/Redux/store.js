dbec1e192c0484fb4ea7525f367d98639f24c36d
Saniru Rajapaksha <sanirurajapaksha456@gmail.com>
2021-09-20 01:13:59 +0530

    main

--- client/memories-app/Redux/store.js
+++ client/memories-app/Redux/store.js
@@ -1,7 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
+import thunk from "redux-thunk";
 import combineReducers from "../Redux/reducers/combine";
 
 export const store = configureStore({
   reducer: combineReducers,
   devTools: true,
+  middleware: [thunk],
 });
