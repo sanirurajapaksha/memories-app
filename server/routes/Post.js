d49ca96e11e64dfa5b71978c0f215594c4f76394
Saniru Rajapaksha <sanirurajapaksha456@gmail.com>
2021-09-23 19:48:09 +0530

    main

--- server/routes/Post.js
+++ server/routes/Post.js
@@ -1,28 +1,10 @@
 import express from "express";
-// import multer from "multer";
 
 import { getPosts, createPosts } from "../controllers/post.js";
 
 const router = express.Router();
 
-// var storage = multer.diskStorage({
-//   destination: function (req, file, cb) {
-//     cb(null, "/filepath");
-//   },
-
-//   filename: function (req, file, cb) {
-//     let filename = "filenametogive";
-//     req.body.file = filename;
-
-//     cb(null, filename);
-//   },
-// });
-
-// const upload = multer({ storage: storage });
-
 router.get("/", getPosts);
 router.post("/", createPosts);
 
 export default router;
-
-// upload.single("filename"),
