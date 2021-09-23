979f3a9e9788555935e6c7e427b98c5df6030ca0
Saniru Rajapaksha <sanirurajapaksha456@gmail.com>
2021-09-23 19:50:35 +0530

    main

--- client/memories-app/Redux/reducers/postReducers.js
+++ client/memories-app/Redux/reducers/postReducers.js
@@ -3,7 +3,7 @@ const initialState = [];
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case "post/create":
-      console.log(action.payload);
+      console.log(action.payload); // debugging line
       return [...state, action.payload];
 
     case "post/get":
