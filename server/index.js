14cfd29b3f1225c3ad5e533404ba417e8e29dd12
Saniru Rajapaksha <sanirurajapaksha456@gmail.com>
2021-09-23 19:41:43 +0530

    main

--- server/index.js
+++ server/index.js
@@ -6,8 +6,8 @@ import router from "../server/routes/Post.js";
 
 const app = express();
 
-app.use(express.json({ extend: true }));
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: "200mb", extend: true }));
+app.use(express.urlencoded({ limit: "200mb", extended: true }));
 app.use(cors());
 
 app.use("/posts", router);
