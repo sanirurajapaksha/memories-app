c7dfba7f9dd4ae4a616e6ebd0633fa474d9bdf47
Saniru Rajapaksha <sanirurajapaksha456@gmail.com>
2021-09-24 01:58:00 +0530

    main

--- client/memories-app/Redux/actions/postActions.js
+++ client/memories-app/Redux/actions/postActions.js
@@ -4,16 +4,18 @@ import axios from "axios";
 export const getPosts = () => async (dispatch) => {
   // 1. Send the GET request to server and get the data
 
-  const data = await axios
+  var postData = null;
+  await axios
     .get("http://localhost:8080/posts/")
-    .then((postData) => postData.data)
+    .then((data) => (postData = data.data))
+    .then(() => console.log(postData))
     .catch((err) => console.log(err));
 
   // 2. Return Redux state immediately with Updated Data
 
   dispatch({
     type: "post/get",
-    payload: data,
+    payload: postData,
   });
 };
 
