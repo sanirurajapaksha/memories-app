02d462086b491904415c315f54a03763fae78396
Saniru Rajapaksha <sanirurajapaksha456@gmail.com>
2021-09-28 19:13:07 +0530

    main

--- client/memories-app/components/body.js
+++ client/memories-app/components/body.js
@@ -9,9 +9,7 @@ function Body() {
     <div className="flex w-full h-full bg-gray-600">
       <div className="flex flex-row mt-28">
         {!postsFromRedux.length ? (
-          <div className="flex w-full h-full">
-            <Post postState={postsFromRedux} />
-          </div>
+          <Post postState={postsFromRedux} />
         ) : (
           <>
             <Post postState={postsFromRedux} />
