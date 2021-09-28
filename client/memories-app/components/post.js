b94284d5b6d77fad828fffdc35a5105a80c24577
Saniru Rajapaksha <sanirurajapaksha456@gmail.com>
2021-09-29 03:38:59 +0530

    main

--- client/memories-app/components/post.js
+++ client/memories-app/components/post.js
@@ -10,7 +10,7 @@ function Post(props) {
       <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-6 mt-10 ml-9">
         {postane.map((post, i) => (
           <div key={i}>
-            <div className="flex flex-col w-80 h-96 bg-white rounded-2xl">
+            <div className="flex flex-col w-80 h-96 bg-gray-300 rounded-2xl">
               <div className="flex flex-row w-full h-8 justify-between items-center">
                 <div className="ml-3">
                   <h3 className="font-sans text-gray-700">{post.creator}</h3>
