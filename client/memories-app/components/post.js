0b8aa2a05f5a6dc87a5e7e38e4031f9fe78126f0
Saniru Rajapaksha <sanirurajapaksha456@gmail.com>
2021-09-28 19:19:55 +0530

    main

--- client/memories-app/components/post.js
+++ client/memories-app/components/post.js
@@ -3,7 +3,7 @@ import { CircularProgress } from "@material-ui/core";
 
 function Post(props) {
   return !props.postState.length ? (
-    <CircularProgress />
+    <CircularProgress color="inherit" size={100} />
   ) : (
     <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-6 mt-10 ml-9"></div>
   );
